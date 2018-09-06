c109103545ac1d9421b0477881efac0ccea8e110
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-09-06 21:32:41 +0900

    style: 設定を見やすくする

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
         name: 'pages',
+        path: `${__dirname}/src/pages`,
       },
     },
     `gatsby-transformer-remark`,
