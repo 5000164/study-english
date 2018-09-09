8f119aae6e2d5f767b6cacfc716f4575b9b91545
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-09-09 20:54:31 +0900

    chore: test

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
       resolve: `gatsby-plugin-offline`,
       options: {
         modifyUrlPrefix: {
-          rootDir: `study-english/`,
+          rootDir: ``,
         },
       },
     },
