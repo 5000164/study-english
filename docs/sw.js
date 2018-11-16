6f17888816f0a12b5b754114adfe0d05feb0526f
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-11-16 23:41:25 +1100

    feat: add blog post "English words 2018/11/16"

--- docs/sw.js
+++ docs/sw.js
@@ -25,7 +25,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a34e659ae31bf4b84565.js"
+    "url": "webpack-runtime-3a543be4481ef4e06d6c.js"
   },
   {
     "url": "app-2730aa2ac960051ace17.js"
@@ -35,11 +35,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "d065eeb5e39a8881b7cebf2d8c47d75c"
+    "revision": "9cf40448ab1f126e79de4ccacae1ff2d"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a5f9b830c66d5147d70bae3c0f3e3101"
+    "revision": "6fc1a2961dd5d298823ecf7bf217f337"
   },
   {
     "url": "0-555b3e51084d936a9c9b.js"
@@ -48,8 +48,8 @@ self.__precacheManifest = [
     "url": "component---src-pages-index-js-afaa0e409d2220b943f3.js"
   },
   {
-    "url": "static/d/451/path---index-6a9-3yHMEDrzgOcfNLttq7tDFNuSk8.json",
-    "revision": "ff78e81d99ebdf2cd1ac7da807e5e423"
+    "url": "static/d/709/path---index-6a9-8Kln8JzOpzRs0bKeqiipzUn1k.json",
+    "revision": "995d78c8375b4b37f056d8b8d88ee6b2"
   },
   {
     "url": "component---src-pages-404-js-126e20d22743bc34295b.js"
---
+++ src/pages/2018/11/16/words.md
@@ -0,0 +1,24 @@
+---
+title: "English words 2018/11/16"
+date: "2018-11-16T23:39:26+11:00"
+---
+
+- certain
+    - adjective: 一定
+    - adjective: 確か
+- along
+    - preposition: 沿って
+    - adverb: 伝いに
+- collocation
+    - noun: 連結語句
+    - noun: 連語
+- be on
+    - (… に) 参加する
+    - e.g.: Are you on?
+        - 君も参加しないか？
+- found out
+    - verb: 探し当てる
+    - verb: 見つける
+- adopted
+    - adjective: 養子になった
+    - adjective: 採用された
